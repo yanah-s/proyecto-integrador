4c9f8dd1d6b3d32c27e9ffa1a2182fec0db039d6
yana <yanahinasuarez@gmail.com>
2024-05-19 21:21:53 -0300

    master

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
-// const usuarios = require('./routes/usuarios');
-// const express = require('express');
-// const mongoose = require('mongoose');
-
-// //Conectarnos a la BD LOCAL
-// mongoose.connect('mongodb://localhost:27017/proyectoIntegrador', {useNewUrlParser: true, useUnifiedTopology: true})
-//     .then(() => console.log('Conectado a MongoDB LOCAL!'))
-//     .catch(err => console.log('No se pudo conectar con MongoDB..', err));
-
-
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({extended:true}));
-// app.use('/api/usuarios', usuarios);
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log('Api Ok...');
-// })
-
-
-///////////ori hasta aca 
-
-
-const usuarios = require('./routes/usuarios');
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); 
 
-
-// Configuración de la URL de conexión a la base de datos
-const dbHost = '3.16.90.77';
+const dbHost = 'localhost';
 //'3.16.90.77'; 
 const dbPort = '27017'; 
 const dbName = 'mi_base_de_datos'; 
@@ -43,9 +17,9 @@ mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Conectado a la base de datos '+ servidor))
     .catch(err => console.error('Error al conectar a la base de datos:', err));
 
-
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use('/api/usuarios', usuarios);
 
--- readme.txt
+++ readme.txt
@@ -1,9 +1,5 @@
-Dependencias instaladas:
 
-express
-hapi
-mongoose
-nodemon
--------------------------------------
 
+Configuracion servidor de prueba
 cambie a 0.0.0.0 el archivo conf de mongod en el servidor para que permita conexiones de ip
+recordar agregar reglas de entrada y salida para los puertos y sobre todo 21017 que es el de mongod
